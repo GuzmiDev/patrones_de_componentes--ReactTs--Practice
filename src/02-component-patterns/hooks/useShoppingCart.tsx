4e4717b11293880a6b2be8ec57939d0216f8df4c
Erick Guzman <guzmi.dev@gmail.com>
2022-05-14 11:28:20 -0600

    :art: Control props alt

--- src/02-component-patterns/hooks/useShoppingCart.tsx
+++ src/02-component-patterns/hooks/useShoppingCart.tsx
@@ -1,35 +1,19 @@
 import { useState } from "react";
-import { cart, ProductInCart, onChangeArgs } from "../interfaces/interfaces";
+import { cart, onChangeArgs } from "../interfaces/interfaces";
 export const useShoppingCart = () => {
   const [shoppingCart, setShoppingCart] = useState<cart>();
 
   const onProductCountChange = ({ count, product }: onChangeArgs) => {
     setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart?.[product.id] || {
-        ...product,
-        count: 0,
-      };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest }: any = oldShoppingCart;
+        return rest;
       }
 
-      const { [product.id]: toDelete, ...rest }: any = oldShoppingCart;
-      return rest;
-
-      // if (count === 0) {
-      //   const { [product.id]: toDelete, ...rest }: any = oldShoppingCart;
-      //   return rest;
-      // }
-
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: { ...product, count },
-      // };
+      return {
+        ...oldShoppingCart,
+        [product.id]: { ...product, count },
+      };
     });
   };
 
